bc7d65511d89661a183c6cc9025945b5641729d2
Rishikesh Gajam <rishi.gajam@gmail.com>
2017-11-06 00:10:04 -0800

    Added header & footer partials

--- todos/views.py
+++ todos/views.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-
 from django.shortcuts import render
 from .models import Todo
 
 # Create your views here.
+def hello(request):
+    return HttpResponse('Hello World!')
 
 def index(request):
     todos = Todo.objects.all()[:10]
 
-    # return HttpResponse('Hello World!')
     context = {
         'todos': todos
     }
     return render(request, 'index.html', context)
+
+def details(request, id):
+    todo = Todo.objects.get(id=id)
+
+    context = {
+        'todo': todo
+    }
+    return render(request, 'details.html', context)
